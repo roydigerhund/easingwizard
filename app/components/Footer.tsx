274c8288d2b6c22acb52b5ae3b308c267c5a379c
Matthias Martin <github@stichwort-m.de>
2024-11-24 02:31:50 +0100

    new footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,49 @@
 import { classNames } from '~/utils/class-names';
-import { shortTransition } from '~/utils/common-classes';
-import HeartIcon from './icons/HeartIcon';
+import BlueskyIcon from './icons/BlueskyIcon';
+import XIcon from './icons/XIcon';
 
 export default function Footer() {
   return (
-    <div className="flex items-center justify-center gap-3 text-center text-sm text-zinc-400">
-      <span>Made with </span>
-      <span className="sr-only">love</span>
-      <HeartIcon className="size-6 text-grdt-to drop-shadow-[0_0_0.5rem_var(--grdt-to)]" />
-      <span>
-        by{' '}
-        <a
-          href="https://x.com/RoyDigerhund"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={classNames('text-zinc-100 hover:text-grdt-to', shortTransition)}
-        >
-          Matthias Martin
-        </a>
-      </span>
+    <div className="space-y-6">
+      <hr
+        className="my-5 border-t border-zinc-700"
+        style={{
+          maskImage: 'linear-gradient(to right,rgba(0,0,0,1),rgba(0,0,0,0.1))',
+        }}
+      />
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex items-center justify-center gap-3">
+          <a
+            href="https://bsky.app/profile/matthiasmartin.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classNames('group p-1')}
+          >
+            <span className="sr-only">Profile on Bluesky</span>
+            <BlueskyIcon
+              className={classNames(
+                'size-7 text-zinc-300 group-hover:text-grdt-to group-hover:drop-shadow-[0_0_0.5rem_var(--grdt-to)]',
+                'transition-all duration-300 ease-out',
+              )}
+            />
+          </a>
+          <a
+            href="https://x.com/RoyDigerhund"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={classNames('group p-1')}
+          >
+            <span className="sr-only">Profile on X</span>
+            <XIcon
+              className={classNames(
+                'size-7 text-zinc-300 group-hover:text-grdt-to group-hover:drop-shadow-[0_0_0.5rem_var(--grdt-to)]',
+                'transition-all duration-300 ease-out',
+              )}
+            />
+          </a>
+        </div>
+        <div className="text-sm text-zinc-400">Made by Matthias Martin</div>
+      </div>
     </div>
   );
 }
