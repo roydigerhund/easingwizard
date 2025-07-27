fada6c1d1fbc597758c3ba6e334d1d6d03581767
Matthias Martin <matthias@stichwort-m.de>
2025-07-28 01:41:37 +0200

    fix: reduced openapi document for vercel debugging

--- apps/api/src/routes/openapi.ts
+++ apps/api/src/routes/openapi.ts
@@ -1,16 +1,7 @@
 import { createMarkdownFromOpenApi } from '@scalar/openapi-to-markdown';
-import {
-  API_VERSION,
-  EasingCurveResponseSchema,
-  EasingTypeSchema,
-  ErrorResponseSchema,
-  healthCheckSchema,
-  InputUnionSchema,
-  PresetsResponseSchema,
-} from 'easing-wizard-core';
+import { ErrorResponseSchema, healthCheckSchema } from 'easing-wizard-core';
 import { Hono } from 'hono';
 import { createDocument } from 'zod-openapi';
-import z from 'zod/v4';
 
 const badRequest = {
   description: '400 Bad Request',
@@ -47,80 +38,80 @@ const document = createDocument({
         tags: ['Health'],
       },
     },
-    [`/${API_VERSION}/presets`]: {
-      get: {
-        operationId: 'getPresets',
-        summary: 'Get Preset Curves',
-        description: 'Retrieve a list of available preset easing curves',
-        requestParams: { query: z.object({ type: EasingTypeSchema.optional() }) },
-        responses: {
-          '200': {
-            description: '200 OK',
-            content: {
-              'application/json': { schema: PresetsResponseSchema },
-            },
-          },
-          '400': badRequest,
-        },
-        tags: ['Curves'],
-      },
-    },
-    [`/${API_VERSION}/curves/{id}`]: {
-      get: {
-        operationId: 'getCurveById',
-        summary: 'Get Curve by ID',
-        description: 'Retrieve a specific easing curve by its ID',
-        requestParams: {
-          path: z.object({
-            id: z.string().describe('The ID of the easing curve'),
-          }),
-        },
-        responses: {
-          '200': {
-            description: '200 OK',
-            content: {
-              'application/json': {
-                schema: EasingCurveResponseSchema,
-              },
-            },
-          },
-          '400': badRequest,
-        },
-        tags: ['Curves'],
-      },
-    },
-    [`/${API_VERSION}/curves/{type}`]: {
-      post: {
-        operationId: 'createCurve',
-        summary: 'Create a New Curve',
-        description: 'Generate a new easing curve based on input parameters',
-        requestParams: {
-          path: z.object({
-            type: EasingTypeSchema,
-          }),
-        },
-        requestBody: {
-          required: true,
-          content: {
-            'application/json': {
-              schema: InputUnionSchema.describe('Input parameters for the easing curve'),
-            },
-          },
-        },
-        responses: {
-          '200': {
-            description: '200 OK',
-            content: {
-              'application/json': {
-                schema: EasingCurveResponseSchema,
-              },
-            },
-          },
-          '400': badRequest,
-        },
-        tags: ['Curves'],
-      },
-    },
+    // [`/${API_VERSION}/presets`]: {
+    //   get: {
+    //     operationId: 'getPresets',
+    //     summary: 'Get Preset Curves',
+    //     description: 'Retrieve a list of available preset easing curves',
+    //     requestParams: { query: z.object({ type: EasingTypeSchema.optional() }) },
+    //     responses: {
+    //       '200': {
+    //         description: '200 OK',
+    //         content: {
+    //           'application/json': { schema: PresetsResponseSchema },
+    //         },
+    //       },
+    //       '400': badRequest,
+    //     },
+    //     tags: ['Curves'],
+    //   },
+    // },
+    // [`/${API_VERSION}/curves/{id}`]: {
+    //   get: {
+    //     operationId: 'getCurveById',
+    //     summary: 'Get Curve by ID',
+    //     description: 'Retrieve a specific easing curve by its ID',
+    //     requestParams: {
+    //       path: z.object({
+    //         id: z.string().describe('The ID of the easing curve'),
+    //       }),
+    //     },
+    //     responses: {
+    //       '200': {
+    //         description: '200 OK',
+    //         content: {
+    //           'application/json': {
+    //             schema: EasingCurveResponseSchema,
+    //           },
+    //         },
+    //       },
+    //       '400': badRequest,
+    //     },
+    //     tags: ['Curves'],
+    //   },
+    // },
+    // [`/${API_VERSION}/curves/{type}`]: {
+    //   post: {
+    //     operationId: 'createCurve',
+    //     summary: 'Create a New Curve',
+    //     description: 'Generate a new easing curve based on input parameters',
+    //     requestParams: {
+    //       path: z.object({
+    //         type: EasingTypeSchema,
+    //       }),
+    //     },
+    //     requestBody: {
+    //       required: true,
+    //       content: {
+    //         'application/json': {
+    //           schema: InputUnionSchema.describe('Input parameters for the easing curve'),
+    //         },
+    //       },
+    //     },
+    //     responses: {
+    //       '200': {
+    //         description: '200 OK',
+    //         content: {
+    //           'application/json': {
+    //             schema: EasingCurveResponseSchema,
+    //           },
+    //         },
+    //       },
+    //       '400': badRequest,
+    //     },
+    //     tags: ['Curves'],
+    //   },
+    // },
   },
   tags: [
     { name: 'Curves', description: 'Endpoints for managing easing curves' },
